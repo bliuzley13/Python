bf29871035d913bd84f420121c0eaae00fe1ef1f
Arvind Kumar <44422818+bliuzley13@users.noreply.github.com>
2024-01-08 00:08:26 -0800

    Add files via upload

---
+++ Python Games/hangman/words.py
@@ -0,0 +1,11 @@
+words = [
+    "Serendipity", "Mellifluous", "Ephemeral", "Resplendent", "Effervescent", 
+    "Petrichor", "Luminous", "Sonorous", "Ethereal", "Aurora", "Quintessential", 
+    "Ebullient", "Halcyon", "Epiphany", "Efflorescence", "Evanescent", "Panacea", 
+    "Idyllic", "Nebulous", "Ebullience", "Supine", "Ineffable", "Tranquil", 
+    "Cacophony", "Penumbra", "Solitude", "Sonder", "Ineffable", "Mellifluous", 
+    "Bucolic", "Incandescent", "Gossamer", "Elysian", "Phantasmagorical", 
+    "Labyrinthine", "Susurrus", "Effulgent", "Halcyon", "Evanescent", "Opulent", 
+    "Mellifluous", "Serene", "Chrysalis", "Ephemeral", "Epiphany", "Resplendent", 
+    "Tranquility", "Luminous", "Nostalgia", "Petrichor", "Ethereal", "Aurora"
+]
---
+++ Simple Python Files/selector.py
@@ -0,0 +1,14 @@
+import random
+
+friends = [
+    'Adam',
+    'Alex',
+    'Dave',
+    'Jason',
+    'Edward'
+]
+
+selected = random.choice(friends)
+
+print("Who should I hangout with?")
+print(selected)
