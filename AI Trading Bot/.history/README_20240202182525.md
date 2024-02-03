03083c2a8b21333566e224880245c38897ed38ae
bliuzley13 <arvindkumar.muthukumar@gmail.com>
2024-02-02 18:25:50 -0800

    Project AI

---
+++ AI Trading Bot/.history/README_20240202182525.md
@@ -0,0 +1,5 @@
+Uses a Trading Bot Library
+
+Adjust dates to write information as needed
+
+Uses paper trading, which does not involve real money
---
+++ AI Trading Bot/.history/tradingbot_20240127162750.py
@@ -0,0 +1,4 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import strategy
+from lumibot.traders import Trader
